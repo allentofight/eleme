9582b059777915f461cd7b030179d52a010d7356
xiekun <xiekun605746@163.com>
2017-02-19 22:20:49 +0800

    完成header组件step2

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -35,13 +35,32 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+	@import "../../common/stylus/mixin";
 	.header
 		color: #fff
 		background: #000
 		.content-wrapper
 			padding: 24px 12px 18px 24px
+			font-size: 0
 			.avatar
 				display: inline-block
 			.content
 				display: inline-block
+				margin-left: 16px
+				font-size: 14px
+				.title
+					margin 2px 0 8px 0
+					.brand
+						display: inline-block
+						width: 30px
+						height: 18px
+						bg-image('brand')
+						background-size: 30px 18px
+						background-repeat: no-repeat
+					.name
+						vertical-align: top
+						margin-left: 6px
+						font-size: 16px
+						line-height: 18px
+						font-weight: bold
 </style>
