82802f851eda8f79cd2a79507913b017b2a5e98b
xiekun <xiekun605746@163.com>
2017-04-03 14:51:08 +0800

    初步完成购物车详情页实现

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -22,10 +22,31 @@
 				<div class="inner inner-hook"></div>
 			</div>
 		</div>
+		<div class="shopcart-list" v-show="listShow">
+			<div class="list-header">
+				<h1 class="title">购物车</h1>
+				<span class="empty">清空</span>
+			</div>
+			<div class="list-content">
+				<ul>
+					<li class="food" v-for="food in selectFoods">
+						<span class="name">{{food.name}}</span>
+						<div class="price">
+							<span>￥{{foo.price*foo.count}}</span>
+						</div>
+						<div class="cartcontrol-wrapper">
+							 <cartcontrol :food="food"></cartcontrol>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script type="text/ecmascript-6">
+	import cartcontrol from 'components/cartcontrol/cartcontrol';
+
 	export default {
 		props: {
 			selectFoods: {
@@ -152,6 +173,9 @@
 					}
 				}
 			}
+		},
+		components: {
+			cartcontrol
 		}
 	};
 </script>
