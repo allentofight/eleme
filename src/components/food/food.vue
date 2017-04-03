70d3755c1ae09fe06e19359b34b215d1d9f35d73
xiekun <xiekun605746@163.com>
2017-04-04 00:19:13 +0800

    完成ratingselect组件

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -30,7 +30,7 @@
 			<div class="rating">
 				<h1 class="title">商品评价</h1>
 			</div>
-			<ratingselect :select-type="selectType" :only-content:"onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
+			<ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
 		</div>
 	</div>
 </template>
